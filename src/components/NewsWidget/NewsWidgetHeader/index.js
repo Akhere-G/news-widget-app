13c4e5b9f330b2153af842fb5d76d06f1c7d85e4
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2021-02-23 20:25:18 +0000

    added header functionality and proptypes

--- src/components/NewsWidget/NewsWidgetHeader/index.js
+++ src/components/NewsWidget/NewsWidgetHeader/index.js
@@ -1,7 +1,31 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const NewsWidgetHeader = () => {
-  return <div>news widget header</div>;
+const NewsWidgetHeader = ({ sources, currentSource, setCurrentSource }) => {
+  return (
+    <div>
+      <h4>News</h4>
+      <select
+        value={currentSource}
+        onChange={e => {
+          setCurrentSource(e.target.value);
+        }}
+      >
+        {sources.map(source => {
+          return (
+            <option key={source} value={source}>
+              {source}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
 };
 
+NewsWidgetHeader.propTypes = {
+  sources: PropTypes.arrayOf(PropTypes.string).isRequired,
+  currentSource: PropTypes.string.isRequired,
+  setCurrentSource: PropTypes.func.isRequired,
+};
 export default NewsWidgetHeader;
